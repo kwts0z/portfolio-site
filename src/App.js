df3c07f9270644f615d1ca1772c16d63749a9a32
Kwts0s <kwtsos@kwtsos.kwtsos>
2023-10-17 18:56:20 +0300

    Added vercel web analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { inject } from '@vercel/analytics';
 import Body from './components/body';
 import CardsSection from './components/cardsSection';
 import Footer from './components/footerSection';
 import ProjectsSection from './components/projectsSection';
 import Navbar from './components/navbar';
+import { useEffect } from 'react';
 
 
 function App() {
+  useEffect(() => {
+    inject();
+  }, []);
   return (
     <div id="outer-container" className='h-full overflow-hidden'>
       <header className="App-header">
