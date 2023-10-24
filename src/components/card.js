7754c079bc6924b3687450ed309ee39e526fb61c
Konstantinos <kostas5217@gmail.com>
2023-10-24 20:45:41 +0300

    More project cards added and some fixes on the cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,5 @@
 import "./styles/cardAnimation.css";
+import ReactPlayer from 'react-player';
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import React, { useRef } from 'react';
 import ImageSlider from "./carousel";
@@ -7,7 +8,7 @@ import skoteImage2 from "../assets/images/skote-site-2.png"
 import skoteImage3 from "../assets/images/skote-site-3.png"
 import skoteImage4 from "../assets/images/skote-site-4.png"
 
-function Card({image}) {
+function Card({image, video, title, subtitle}) {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -18,32 +19,48 @@ function Card({image}) {
   const moveUp = useTransform(spring, [0, 1], ["50%", "0%"]);
   const opacity = useTransform(spring, [0, 1], ["0%", "100%"]);
 
-  const slides = [
-    { url: skoteImage, title: "door" },
-    { url: skoteImage2, title: "red carpet door" },
-    { url: skoteImage3, title: "christmas door" },
-    { url: skoteImage4, title: "drop cup" },
-  ];
-
-  return (
-    <motion.div ref={ref} style={{translateY:moveUp, opacity: opacity}}>
-      <div id="card">
-        <div className="relative w-full h-1/2">
-          <ImageSlider slides={slides}/>
-          {/* <img src={image} alt="Project preview" className="rounded-md"></img> */}
-          <div id="card-shape-divider">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" id="shape-fill"></path>
-            </svg>
+  if (image) {
+    return (
+      <motion.div ref={ref} style={{translateY:moveUp, opacity: opacity}}>
+        <div id="card">
+          <div className="relative w-full h-1/2">
+            <ImageSlider slides={image}/>
+            <div id="card-shape-divider">
+              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                  <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" id="shape-fill"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="relative w-full pt-5 px-7 mb-5 space-y-5 overflow-y-auto">
+            <h1 className="text-xl lg:text-3xl font-bold">{title}</h1>
+            <p>{subtitle}</p>
           </div>
         </div>
-        <div className="p-7 space-y-5">
-          <h1 className="text-3xl font-bold">Project</h1>
-          <p>This is the text for the project. It's a small decription for the project</p>
+      </motion.div>
+    );
+  }
+  else {
+    return (
+      <motion.div ref={ref} style={{translateY:moveUp, opacity: opacity}}>
+        <div id="card">
+          <div className="relative w-full h-1/2">
+            <div className="w-full h-full bg-gray-200 rounded-t-md pt-5"><ReactPlayer width='100%' height='90%' url={video} controls={true}/></div>
+            
+            <div id="card-shape-divider">
+              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                  <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" id="shape-fill"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="relative w-full pt-5 px-7 mb-5 space-y-5 overflow-y-auto">
+            <h1 className="text-xl lg:text-3xl font-bold">{title}</h1>
+            <p>{subtitle}</p>
+          </div>
         </div>
-      </div>
-    </motion.div>
-  );
+      </motion.div>
+    );
+  }
+
 }
 
 export default Card;
--- src/components/projectsSection.js
+++ src/components/projectsSection.js
@@ -1,9 +1,17 @@
 import Card from "./card";
-import image from "../assets/images/placeholder.jpg"
 import "./styles/projectSection.css";
-
+import skoteImage from "../assets/images/skote-site-1.png"
+import skoteImage2 from "../assets/images/skote-site-2.png"
+import skoteImage3 from "../assets/images/skote-site-3.png"
+import skoteImage4 from "../assets/images/skote-site-4.png"
 
 function ProjectsSection() {
+  const slides = [
+    { url: skoteImage, title: "door" },
+    { url: skoteImage2, title: "red carpet door" },
+    { url: skoteImage3, title: "christmas door" },
+    { url: skoteImage4, title: "drop cup" },
+  ];
 
   return (
     <div id="projects" className="relative flex flex-col items-center justify-center w-full" >
@@ -12,16 +20,24 @@ function ProjectsSection() {
         <h1 className="text-7xl font-extrabold -z-10 text-text lg:text-9xl">Projects</h1>
       </div>
       <div className="flex flex-col pb-56 w-full justify-center items-center space-y-10">
-        <div className="flex flex-col pt-10 items-center space-y-10 lg:space-y-0 lg:space-x-10 xl:space-x-20 lg:flex-row">
-          <div className="relative lg:top-24"><Card image={image} /></div>
-          <Card image={image}/>
-          <div className="relative lg:top-24"><Card image={image} /></div>
+
+        <div className="flex flex-col pt-10 items-center space-y-10 xl:space-y-0 xl:space-x-20 xl:flex-row">
+          <div className="relative xl:top-24"><Card image={slides} title="Georgios Audio Website" subtitle="A portfolio website for sound designer Georgios."/></div>
+          <Card image={slides} title="This Website" subtitle="This is my portfolio website designed and developed by me."/>
+          <div className="relative xl:top-24"><Card video="https://www.youtube.com/watch?v=g9xeKVPCkvw" title="PikPok" subtitle="PikPok is a simple social media platform like instagram, developed as a university project."/></div>
         </div>
-        <div className="flex flex-col items-center space-y-10 lg:space-y-0 lg:space-x-20 lg:flex-row lg:pt-40">
-          <div className="relative"><Card image={image} /></div>
-          <Card image={image}/>
+
+        <div className="flex flex-col items-center space-y-10 xl:space-y-0 xl:space-x-20 xl:flex-row xl:pt-40">
+          <div className="relative xl:top-24"><Card video="https://youtu.be/E9CtiereuWc" title="OpenGL" subtitle="This university project was developed using OpenGL to create a 3D scene featuring an airplane rotating around the Earth."/></div>
+          <Card video="https://youtu.be/PXRCxz0rN-M" title="Car Washing System" subtitle="This is a server/client app designed to automate the process at a car washing company. This is part of a university project"/>
+          <div className="relative xl:top-24"><Card video="https://youtu.be/6q8DK-yLDlw" title="Weather Forecast App" subtitle="In this university project, I utilized Scrapy for web scraping to collect and filter weather data from three forecast websites based on humidity, temperature, and wind speed."/></div>
+        </div>
+
+        <div className="xl:pt-24">
+          <Card video="https://youtu.be/iywGxnhXyhw" title="Article Search Engine" subtitle="An Apache Lucene-powered search engine for articles, offering advanced search capabilities through diverse fields and the ability to manage articles with ease."/>
         </div>
       </div>
+
       <div id="custom-shape-divider-bottom-1697562712">
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
               <path d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z" id="shape-fill"></path>
