9ac63192e604d8f1755794f670a25f4939d4f076
Konstantinos <kostas5217@gmail.com>
2023-10-31 21:05:42 +0200

    Added download CV

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -44,6 +44,7 @@ function Navbar() {
           <li className="text-white cursor-pointer opacity-100 hover:opacity-60"><a href='#education' onClick={(e) => {e.preventDefault(); scrollToSection('education')}}>Education</a><span className="text-black font-bold">/&gt;</span></li>
           <li className="text-white cursor-pointer opacity-100 hover:opacity-60"><a href='#projects' onClick={(e) => {e.preventDefault(); scrollToSection('projects')}}>Projects</a><span className="text-black font-bold">/&gt;</span></li>
           <li className="text-white cursor-pointer opacity-100 hover:opacity-60"><a href='#footer' onClick={(e) => {e.preventDefault(); scrollToSection('footer')}}>MySocial</a><span className="text-black font-bold">/&gt;</span></li>
+          <li className="text-white cursor-pointer opacity-100 hover:opacity-60"><a href='/myCV.pdf' download>MyCV</a><span className="text-black font-bold">/&gt;</span></li>
         </ul>
       </div>
       <div className={`lg:hidden z-50 flex fixed w-full transition-all duration-300 ${isNavbarVisible ? 'opacity-100 transform-none' : 'opacity-0 -translate-y-full'}`}>
@@ -52,7 +53,8 @@ function Navbar() {
               <li className="text-fuchsia-50 cursor-pointer opacity-100 hover:opacity-60"><a href='#home' onClick={(e) => {e.preventDefault(); scrollToSection('home')}}>Home</a><span className="text-background  font-bold">/&gt;</span></li>
               <li className="text-fuchsia-50 cursor-pointer opacity-100 hover:opacity-60"><a href='#education' onClick={(e) => {e.preventDefault(); scrollToSection('education')}}>Education</a><span className="text-background font-bold">/&gt;</span></li>
               <li className="text-fuchsia-50 cursor-pointer opacity-100 hover:opacity-60"><a href='#projects' onClick={(e) => {e.preventDefault(); scrollToSection('projects')}}>Projects</a><span className="text-background font-bold">/&gt;</span></li>
-              <li className="text-fuchsia-50 cursor-pointer opacity-100 hover:opacity-60"><a href='#footer' onClick={(e) => {e.preventDefault(); scrollToSection('footer')}}>MySocial</a><span className="text-background  font-bold">/&gt;</span></li>
+              <li className="text-fuchsia-50 cursor-pointer opacity-100 hover:opacity-60"><a href='#footer' onClick={(e) => {e.preventDefault(); scrollToSection('footer')}}>MySocial</a><span className="text-background font-bold">/&gt;</span></li>
+              <li className="text-fuchsia-50 cursor-pointer opacity-100 hover:opacity-60"><a href='/myCV.pdf' download>MyCV</a><span className="text-background font-bold">/&gt;</span></li>
             </ul>
           </Menu>
         </div>
