7a88fee0708ba7d9be1d956143351140bee3e8b9
Konstantinos <kostas5217@gmail.com>
2024-02-17 03:07:24 +0200

    divider added

--- src/components/body.js
+++ src/components/body.js
@@ -8,7 +8,6 @@ function Body() {
       <div className="w-full h-screen">
         <NewAnimation/>
       </div>
-
       <div className="absolute flex flex-col w-full z-10 justify-center items-center text-center">
         <div className="flex relative rounded-full bg-white h-64 w-64 md:h-80 md:w-80 overflow-hidden">
           <img className="scale-125 pb-4 pl-3" src={avatar} alt="My avatar" ></img>
@@ -19,6 +18,7 @@ function Body() {
           <span className="text-secondary">"</span>
         </p>
       </div>
+      
     </div>
   );
 }
